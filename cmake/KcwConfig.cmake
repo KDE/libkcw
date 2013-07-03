95454d358a73faeb54c7066e4405c0f06820dda6
Patrick Spendrin <ps_ml@gmx.de>
2013-07-03 23:57:53 +0200

    add a config file to find kcw from outside

---
+++ cmake/KcwConfig.cmake
@@ -0,0 +1,9 @@
+# Config file for kcw
+# It defines the following variables
+# KCW_INCLUDE_DIRS  - include directories for kcw
+# KCW_LIBRARIES     - libraries to link against
+
+get_filename_component(KCW_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+
+set(KCW_INCLUDE_DIRS ${KCW_CMAKE_DIR}/../../../include/kcw)
+set(KCW_LIBRARIES kcw)
